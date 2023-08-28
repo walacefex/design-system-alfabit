7a844760781b22c4e9b1c904cd479414a030370d
walacefex <walacefex01@gmail.com>
2023-08-28 15:27:49 -0300

    creating component Input with option multiline and your variant

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,26 +7,24 @@ export type InputProps = {
 )
 
 const InputOrTextArea = (props: InputProps) => {
-  if(props.multiline) {
+  if (props.multiline) {
     return <textarea {...props as React.TextareaHTMLAttributes<HTMLTextAreaElement>} />
   }
-  return <input {...props as  React.InputHTMLAttributes<HTMLInputElement>} />
+  return <input {...props as React.InputHTMLAttributes<HTMLInputElement>} />
 }
 
-const Input = ({label, className, ...rest}: InputProps) => {
-  return (
-    <div className="relative">
-      {label && <label className="absolute">{label}</label> }
-      <InputOrTextArea
-        {...rest} 
-        className={`
-          bg-dark  text-gray-primary
-          disabled:bg-disabled disabled:border-2 disabled:border-disabled
-          ${className}
-        `}
-      />
-    </div>
-  )
+const Input = ({ label, className, ...rest}: InputProps) => {
+  return <div className="relative">
+    {label && <label className="absolute pt-1 pl-3 text-xs">{label}</label>}
+    <InputOrTextArea
+      className={`
+        bg-dark text-gray-primary rounded-md p-3
+         disabled:bg-disabled disabled:border-2 disabled:border-disabled
+        ${label && 'pt-5'}
+        ${className}
+      `}
+      {...rest}
+    />
+  </div>
 }
-
 export default Input
