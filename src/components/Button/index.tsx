abfbc5bb475ef747ba179886956e5e467e01c8f3
walacefex <walacefex01@gmail.com>
2023-08-28 13:52:39 -0300

    Adding props variant to changed primary, secondary and tertiary

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,20 +1,31 @@
 export type ButtonProps = {
   children: React.ReactNode;
+  variant?: 'primary' | 'secondary' | 'tertiary';
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children, className, disabled, ...rest }: ButtonProps) => {
-  const generalStyle = 'rounded-md px-6 py-2'
-  const Btn = (classes: string) => {
+function getVariant(variant: ButtonProps['variant'], disabled: ButtonProps['disabled']) {
+  switch (variant) {
+    case 'primary':
+      return disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white'
+    case 'secondary':
+     return disabled ? 'bg-disabled text-disabled' : 'bg-quaternary text-primary'
+    case 'tertiary':
+      return disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white'
+    default:
+      return disabled ? 'bg-disabled text-disabled' : ''
+  }
+}
+
+const Button = ({variant = 'primary', children, className, disabled, ...rest }: ButtonProps) => {
+
     return <button
-      className={`${generalStyle} ${classes} ${className}`}
+      className={`rounded-md px-6 py-2 ${getVariant(variant, disabled)} ${className}`}
       disabled={disabled}
       {...rest}
     >
       { children }
     </button>
-  }
 
-  return Btn(disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white')
 }
 
 export default Button;
