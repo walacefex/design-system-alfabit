d9489b039b72301cf37672c046e59bad80b4bd5f
walacefex <walacefex01@gmail.com>
2023-08-28 12:45:23 -0300

    created a function to disable the button and adding the disabled style to it

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,15 +2,19 @@ export type ButtonProps = {
   children: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children, className }: ButtonProps) => {
-  return(
-    <button className={`
-      bg-primary text-white px-6 py-2 rounded-md text-sm
-      ${className}
-    `}>
-      {children}
-    </button>
-  )
+const Button = ({ children, className, disabled, ...rest }: ButtonProps) => {
+  const generalStyle = "rounded-md px-6 py-2"
+  const Btn = (classes: string) => {
+    return( <button 
+        className={`${generalStyle} ${classes} ${className}`}
+        disabled={disabled}
+        {...rest}
+        >
+        {children}
+      </button>
+    )
+  }
+  return Btn(disabled ? "bg-bg-disabled text-text-disabled" : "bg-primary text-white")
 }
 
 export default Button;
